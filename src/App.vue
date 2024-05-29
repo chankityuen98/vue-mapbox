e0bfc9616e5e4de24c0ba068b007d14895246e6d
CKY <ky.chan@up-devlabs.io>
2024-05-29 17:25:17 +0800

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <h1 class="text-green-700">Home page</h1>
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped></style>
