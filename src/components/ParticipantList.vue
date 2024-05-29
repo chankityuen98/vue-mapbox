bd9de1f36b0318086fe1a58a9b79faf678ec2ddf
unknown <chankityuen98@gmail.com>
2024-05-29 23:29:33 +0800

    work on participant list

--- src/components/ParticipantList.vue
+++ src/components/ParticipantList.vue
@@ -1,4 +1,10 @@
-<template></template>
+<template>
+  <div class="bg-white rounded-lg p-3">
+    <div class="">
+      <p class="font-bold">Participants List</p>
+    </div>
+  </div>
+</template>
 
 <script setup>
 import ParticipantCard from './ParticipantCard.vue'
